bbf257984a2263824e01000560d4aac9723f9c62
Shadow <admin@hongyuvip.com>
2017-10-19 12:50:42 +0800

    HongYuJD - V7.9.1 修复用户中心待发货订单不显示分页按钮问题

--- user.php
+++ user.php
@@ -1958,7 +1958,7 @@ function action_order_list ()
 	/* 待发货的订单： */
 	$order_count['await_ship'] = $db->GetOne('SELECT COUNT(*)' . ' FROM ' . $ecs->table('order_info') . " WHERE 1 $ex_where " . order_query_sql('await_ship'));
 	$status['await_ship'] = CS_AWAIT_SHIP;
-	var_dump($order_count['await_ship']);
+
 	/* 待付款的订单： */
 	$order_count['await_pay'] = $db->GetOne('SELECT COUNT(*)' . ' FROM ' . $ecs->table('order_info') . " WHERE 1 $ex_where " . order_query_sql('await_pay'));
 	$status['await_pay'] = CS_AWAIT_PAY;
