bea04d1ee6eb4a4f81bdb09f948d79062ce271fe
HongYuKeJi <hongyukeji@126.com>
2017-12-26 17:31:34 +0800

    HongYuJD - V7.9.5

--- mobile/pay/alipayapi.php
+++ mobile/pay/alipayapi.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html>
 
 <head>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-	<title>支付宝即时到账交易接口接口</title>
+    <title>支付宝即时到账交易接口接口</title>
 
 </head>
-
 
 
 <?php
@@ -25,9 +25,7 @@ require_once('../includes/init.php');
 require_once('../includes/lib_order.php');
 
 
-
-
-$sql = 'SELECT * FROM ' . $GLOBALS['ecs']->table('ecsmart_payment') ." WHERE pay_code = 'alipay'";
+$sql = 'SELECT * FROM ' . $GLOBALS['ecs']->table('ecsmart_payment') . " WHERE pay_code = 'alipay'";
 
 
 $payment = $GLOBALS['db']->getRow($sql);
@@ -36,17 +34,10 @@ $payment = $GLOBALS['db']->getRow($sql);
 $p_config = unserialize_config($payment['pay_config']);
 
 
-
 $alipay_config = get_alipay_config($p_config);
 
 
-
-
-
-
-
 /**************************调用授权接口alipay.wap.trade.create.direct获取授权码token**************************/
-
 
 
 //返回格式
@@ -56,13 +47,11 @@ $format = "xml";
 //必填，不需要修改
 
 
-
 //返回格式
 
 $v = "2.0";
 
 //必填，不需要修改
-
 
 
 //请求号
@@ -72,27 +61,23 @@ $req_id = date('Ymdhis');
 //必填，须保证每次请求都是唯一
 
 
-
 //**req_data详细信息**
-
 
 
 //服务器异步通知页面路径
 
-$notify_url = "http://".$_SERVER["HTTP_HOST"]."/mobile/pay/ajax_url.php";
+$notify_url = "http://" . $_SERVER["HTTP_HOST"] . "/mobile/pay/ajax_url.php";
 
 //$notify_url = "http://www.duudoo.com/test.asp";
 
 //需http://格式的完整路径，不允许加?id=123这类自定义参数
 
 
-
 //页面跳转同步通知页面路径
 
-$call_back_url = "http://".$_SERVER["HTTP_HOST"]."/mobile/pay/result_url.php";
+$call_back_url = "http://" . $_SERVER["HTTP_HOST"] . "/mobile/pay/result_url.php";
 
 //需http://格式的完整路径，不允许加?id=123这类自定义参数
-
 
 
 //卖家支付宝帐户
@@ -102,13 +87,11 @@ $seller_email = $p_config['alipay_account'];
 //必填
 
 
-
 //商户订单号
 
 $out_trade_no = $_GET['out_trade_no'];
 
 //商户网站订单系统中唯一订单号，必填
-
 
 
 //订单名称
@@ -118,7 +101,6 @@ $subject = $_GET['out_trade_no'];
 //必填
 
 
-
 //付款金额
 
 $total_fee = $_GET['total_fee'];
@@ -126,6 +108,14 @@ $total_fee = $_GET['total_fee'];
 //必填
 
 
+//验证金额
+define('IN_ECS', true);
+require_once('../includes/init.php');
+$sql = "SELECT order_amount FROM " . $GLOBALS['ecs']->table('order_info') . " where order_sn='$out_trade_no'";
+$order['order_amount'] = $GLOBALS['db']->getOne($sql);
+if ($order['order_amount'] != $total_fee) {
+    $total_fee = $order['order_amount'];
+}
 
 //请求业务参数详细
 
@@ -134,33 +124,30 @@ $req_data = '<direct_trade_create_req><notify_url>' . $notify_url . '</notify_ur
 //必填
 
 
-
 /************************************************************/
-
 
 
 //构造要请求的参数数组，无需改动
 
 $para_token = array(
 
-		"service" => "alipay.wap.trade.create.direct",
+    "service" => "alipay.wap.trade.create.direct",
 
-		"partner" => trim($alipay_config['partner']),
+    "partner" => trim($alipay_config['partner']),
 
-		"sec_id" => trim($alipay_config['sign_type']),
+    "sec_id" => trim($alipay_config['sign_type']),
 
-		"format"	=> $format,
+    "format" => $format,
 
-		"v"	=> $v,
+    "v" => $v,
 
-		"req_id"	=> $req_id,
+    "req_id" => $req_id,
 
-		"req_data"	=> $req_data,
+    "req_data" => $req_data,
 
-		"_input_charset"	=> trim(strtolower($alipay_config['input_charset']))
+    "_input_charset" => trim(strtolower($alipay_config['input_charset']))
 
 );
-
 
 
 //建立请求
@@ -170,11 +157,9 @@ $alipaySubmit = new AlipaySubmit($alipay_config);
 $html_text = $alipaySubmit->buildRequestHttp($para_token);
 
 
-
 //URLDECODE返回的信息
 
 $html_text = urldecode($html_text);
-
 
 
 //解析远程模拟提交后返回的信息
@@ -182,17 +167,12 @@ $html_text = urldecode($html_text);
 $para_html_text = $alipaySubmit->parseResponse($html_text);
 
 
-
 //获取request_token
 
 $request_token = $para_html_text['request_token'];
 
 
-
-
-
 /**************************根据授权码token调用交易接口alipay.wap.auth.authAndExecute**************************/
-
 
 
 //业务详细
@@ -202,29 +182,27 @@ $req_data = '<auth_and_execute_req><request_token>' . $request_token . '</reques
 //必填
 
 
-
 //构造要请求的参数数组，无需改动
 
 $parameter = array(
 
-		"service" => "alipay.wap.auth.authAndExecute",
+    "service" => "alipay.wap.auth.authAndExecute",
 
-		"partner" => trim($alipay_config['partner']),
+    "partner" => trim($alipay_config['partner']),
 
-		"v"	=> $v,
+    "v" => $v,
 
-		"sec_id" => trim($alipay_config['sign_type']),
+    "sec_id" => trim($alipay_config['sign_type']),
 
-		"format"	=> $format,
+    "format" => $format,
 
-		"req_id"	=> $req_id,
+    "req_id" => $req_id,
 
-		"req_data"	=> $req_data,
+    "req_data" => $req_data,
 
-		"_input_charset"	=> trim(strtolower($alipay_config['input_charset']))
+    "_input_charset" => trim(strtolower($alipay_config['input_charset']))
 
 );
-
 
 
 //建立请求
